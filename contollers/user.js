36b3bdae6c18bd5da30de61cc4795a7886e3c8df
abdulrehman41 <ranaabdulrehman7588@gmail.com>
2023-05-01 22:28:24 +0500

    fix user cards data query

--- contollers/user.js
+++ contollers/user.js
@@ -5,43 +5,26 @@ const pool = createPool({
     user: "root",
     password: "",
     database: "blood",
-    connectionLimit: 10
+    connectionLimit: 10,
+    multipleStatements:true
 })
 module.exports = {
     cardsData: async (req, res) => {
-
-        var total_patient = 0;
-        var total_donor = 0;
-        var total_request = 0;
-        var total_events = 0;
-
-
-        const sql = `SELECT count(*) as patient_count from user WHERE user_role = '2'`;
-        pool.query(sql, (err, result, field) => {
-            total_patient = result[0].patient_count
-
-            const sql2 = `SELECT count(*) as donor_count from user WHERE user_role = '3'`;
-            pool.query(sql2, (err, result, field) => {
-                total_donor = result[0].donor_count
-            })
-
-            const sql3 = `SELECT count(*) as total_request FROM requests`;
-            pool.query(sql3, (err, result, field) => {
-                total_request = result[0].total_request
-            })
-
-            const sql4 = `SELECT count(*) as total_events FROM events`;
-            pool.query(sql4, (err, result, field) => {
-                total_events = result[0].total_events
-                return res.json({
-                    'total_patients': total_patient,
-                    'total_donors': total_donor,
-                    'total_requests': total_request,
-                    'total_events': total_events
-                })
+        const sql = `SELECT count(*) as patient_count from user WHERE user_role = '2';SELECT count(*) as donor_count from user WHERE user_role = '3';
+        SELECT count(*) as total_request FROM requests;SELECT count(*) as total_events FROM events`;
+        pool.query(sql, (err, results, fields) => {
+            console.log(results[1][0].donor_count);
+            const total_patient = results[0][0].patient_count;
+            const total_donor = results[1][0].donor_count;
+            const total_request = results[2][0].total_request;
+            const total_events = results[3][0].total_events;
+            return res.json({
+                'total_patients': total_patient,
+                'total_donors': total_donor,
+                'total_requests': total_request,
+                'total_events': total_events
             })
         })
-
     },
     addUser: async(req,res) => {
         const user_data = `SELECT * FROM user WHERE email = '${req.body.email}'`;
@@ -140,6 +123,17 @@ module.exports = {
                     return res.json({status:1,count:count,msg:"No data found"})
                 }
             })
+        })
+    },
+    bulkDeleteUser: async(req,res) => {
+        
+        const ids = "("+req.body.deleteData.toString()+")";
+        const sql = `Update user SET archive = 1 WHERE id IN ${ids}`;
+        pool.query(sql,(err,result,fields)=>{
+            if(err)
+                return res.json({status:1,msg:err});
+            else
+                return res.json({status:2,msg:"User deleted successfully!!"});
         })
     }
 }
