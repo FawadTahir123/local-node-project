748e1d65e1bc1b0acc1c2e44ba1768e4c0de1d4e
abdulrehman41 <ranaabdulrehman7588@gmail.com>
2023-05-01 14:41:49 +0500

    add apis for user page

--- contollers/user.js
+++ contollers/user.js
@@ -42,5 +42,102 @@ module.exports = {
             })
         })
 
+    },
+    addUser: async(req,res) => {
+        const user_data = `SELECT * FROM user WHERE email = '${req.body.email}'`;
+        pool.query(user_data,(err,result,fields) => {
+            if(Object.keys(result).length>0)
+            {
+                return res.json({status:0,msg:"Email Already exist"});
+            }
+            else {
+
+                var ciphertext = CryptoJS.AES.encrypt(req.body.password, 'jkhkefheuf398rubjkvebkeejvn').toString();
+
+                const sql = `INSERT INTO user 
+                (first_name,last_name,cnic,phone_no,age,blood_group,email,password,user_role,status,address,gender,availability) 
+                VALUES ('${req.body.firstName}','${req.body.lastName}','${req.body.cnic}','${req.body.phoneNo}','${req.body.age}',
+                '${req.body.blood}','${req.body.email}','${ciphertext}','${req.body.user_role}','${req.body.donor_status}','${req.body.address}',
+                '${req.body.gender}','')`;
+                pool.query(sql,(err,result,fields) => {
+                    if(err)
+                    {
+                        return res.json({status:1,msg:err})
+                    }
+                    else {
+                        return res.json({status:2,msg:'User registered successfully!!'})
+                    }
+                })
+            }
+        })
+    },
+    editUser: async(req,res) => {
+        const {id} = req.params;
+        console.log(id);
+        const sql = `UPDATE user SET first_name = '${req.body.firstName}', last_name = '${req.body.lastName}',
+        cnic = '${req.body.cnic}', phone_no = '${req.body.phoneNo}', age = '${req.body.age}',blood_group = '${req.body.blood}',
+        user_role = '${req.body.user_role}', status = '${req.body.donor_status}', address = '${req.body.address}', gender = '${req.body.gender}',
+        availability = '' WHERE id = '${id}'`;
+
+        pool.query(sql,(err,result,fields) => {
+            if(err)
+            {
+                return res.json({status:1,msg:err});
+            }
+            else {
+                return res.json({status:2,msg:"User update successfully!!"});
+            }
+        })
+    },
+    deleteUser: async(req,res) => {
+        const {id} = req.params;
+        const sql = `DELETE FROM user where id = '${id}'`;
+        pool.query(sql,(err,result,fields)=>{
+            if(err)
+            {
+                return res.json({status:1,msg:err});
+            }
+            else {
+                return res.json({status:2,msg:"User deleted Successfully!!"});
+            }
+        })
+    },
+    viewUser: async(req,res)=>{
+        const {id} = req.params;
+        const sql = `SELECT * FROM user WHERE id = '${id}'`;
+        pool.query(sql,(err,result,fields)=>{
+            if(err)
+            {
+                return res.json({status:1,msg:err});
+            }
+            else if(Object.keys(result).length>0)
+            {
+                return res.json({status:2,data:result})
+            }
+            else {
+                return res.json({status:1,msg:"No data found"})
+            }
+        })
+    },
+    getAllUser: async(req,res)=>{
+        const {page,limit} = req.query;
+        const sql2 = `SELECT count(*) as user_count FROM user`;
+        pool.query(sql2,(err,result,fields)=>{
+            const sql = `SELECT * FROM user Limit ${limit} offset ${(page-1)*limit}`;
+            const count = result[0].user_count;
+            pool.query(sql,(err,result,fields)=>{
+                if(err)
+                {
+                    return res.json({status:1,msg:err});
+                }
+                else if(Object.keys(result).length>0)
+                {
+                    return res.json({status:2,count:count,data:result})
+                }
+                else {
+                    return res.json({status:1,count:count,msg:"No data found"})
+                }
+            })
+        })
     }
 }
--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,12 @@ const {Request} = require('../contollers')
 
 
 apiRoutes.post('/request-blood/:id',Request.requestBlood);
+//User Controller
 apiRoutes.get('/get-cards-data',User.cardsData);
+apiRoutes.post('/add-user',User.addUser);
+apiRoutes.put('/edit-user/:id',User.editUser);
+apiRoutes.delete('/delete-user/:id',User.deleteUser);
+apiRoutes.get('/view-user/:id',User.viewUser);
+apiRoutes.get('/get-all-user',User.getAllUser);
+//End User Controller
 module.exports = apiRoutes;
